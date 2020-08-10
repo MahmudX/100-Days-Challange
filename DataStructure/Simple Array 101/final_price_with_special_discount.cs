2067bfa97a98b17cad507ab7d49d871863ad7da9
Mahmudul Hasan <mail@mahmudnotes.com>
2020-08-10 06:14:26 +0000

    Solved another easy leetcode problem

---
+++ DataStructure/Simple Array 101/final_price_with_special_discount.cs
@@ -0,0 +1,27 @@
+using System;
+
+class MainClass {
+  
+  public static void Main (string[] args) {
+    //https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+    int[] arr = new int[]{8,4,6,2,3};
+    foreach(var item in FinalPrices(arr))
+    {
+      Console.WriteLine(item);
+    }
+  }
+public static int[] FinalPrices(int[] prices) {       
+        for(int i = 0; i < prices.Length - 1; i++)
+        {
+          for(int j = i + 1; j < prices.Length - 1; j++)
+          {
+            if(prices[i] > prices[j])
+            {
+                prices[i] = prices[i] - prices[j];
+                break;
+            }
+          }
+        }
+        return prices;
+    }
+}
