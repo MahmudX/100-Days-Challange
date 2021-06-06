5139fd8592ac19c710ad7c7f3df734ab5945fea5
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-06-06 06:24:07 +0600

    Solved 'https://codeforces.com/contest/1535/problem/A'

---
+++ fair_playoff.py
@@ -0,0 +1,22 @@
+def GetGreater(twinList: list) -> int:
+    if twinList[0] > twinList[1]:
+        return twinList[0]
+    else:
+        return twinList[1]
+
+
+test_case = int(input())
+
+for _ in range(test_case):
+    inputs = list(map(int, input().split()))
+    first_pair = inputs[:2]
+    second_pair = inputs[2:]
+    inputs = sorted(inputs)
+    first_winner = GetGreater(first_pair)
+    second_winner = GetGreater(second_pair)
+    final = sorted([first_winner, second_winner])
+    if final == inputs[2:]:
+        print("Yes")
+    else:
+        print("No")
+    
